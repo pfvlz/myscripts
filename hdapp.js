77b0c52b6f3a4d17a127396b9d5f39c24ee4334a
小朋 <2810580330@qq.com>
2021-11-01 16:21:31 +0800

    Create hdapp.js

---
+++ hdapp.js
@@ -0,0 +1,12 @@
+/*
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+注册地址：https://ls.hzyuechuang.com/#/register
+不含邀请链接
+需要实名认证后绑定支付宝
+xgPhone:手机号#密码
+export xgPhone='手机号#密码'
+boxjs地址:https://raw.fastgit.org/byxiaopeng/myscripts/main/byxiaopeng.boxjs.json
+*/
+// [task_local]
+//#鸿答
+// 9 11 * * * https://raw.fastgit.org/byxiaopeng/myscripts/main/hdapp.js, tag=鸿答, enabled=true
